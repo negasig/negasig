33dba31f9db15006a7c7ac81593dba6d0d456e5b
Negasi Gidena <Negasig21@gmail.com>
2021-10-29 15:44:36 +0300

    hello

--- src/main/java/myprogram/Greetinng.java
+++ src/main/java/myprogram/Greetinng.java
@@ -5,6 +5,10 @@ public class Greetinng {
 	public static void main(String[] args) {
 	System.out.println("Hello Worled");
 //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
+	int a=8;
+	int b=2;
+	int c=a+b;
+	System.out.println(c);
 	}
 
 }
